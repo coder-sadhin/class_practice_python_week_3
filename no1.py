b307358dfa0aebb6eb391db9173f98a120e8e084
akkas_dev <codersadhin@gmail.com>
2024-06-26 21:04:23 +0800

    no 1 problem

---
+++ no1.py
@@ -0,0 +1,8 @@
+def arrange(s):
+    lowercase = ''.join(c for c in s if c.islower())
+    uppercase = ''.join(c for c in s if c.isupper())
+    return lowercase + uppercase
+
+input_string = input()
+result = arrange(input_string)
+print(result)
