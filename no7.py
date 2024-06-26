1d83a4ee59e8576c535656e80acd8cfdac2dfd1f
akkas_dev <codersadhin@gmail.com>
2024-06-26 21:05:39 +0800

    no 7 problem

---
+++ no7.py
@@ -0,0 +1,28 @@
+# Find words with both alphabets and numbers
+# Description
+# Write a program to find words with both alphabets and numbers from an input string.‪‬‪‬‪‬‪‬‪‬‮‬‪‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‪‬‪‬‪‬‪‬‪‬‪‬‮‬‭‬‪‬‪‬‪‬‪‬‪‬‪‬‮‬‭‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‭‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‪‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‭‬
+
+# Hint‪‬‪‬‪‬‪‬‪‬‮‬‪‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‪‬‪‬‪‬‪‬‪‬‪‬‮‬‭‬‪‬‪‬‪‬‪‬‪‬‪‬‮‬‭‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‭‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‪‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‭‬
+
+# Use the built-in function any() with the combination of string functions isalpha() and isdigit()‪‬‪‬‪‬‪‬‪‬‮‬‪‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‪‬‪‬‪‬‪‬‪‬‪‬‮‬‭‬‪‬‪‬‪‬‪‬‪‬‪‬‮‬‭‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‭‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‪‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‭‬
+
+# Example
+# Input
+# Emma25 is Data scientist50 and AI Expert
+
+# Output
+# Emma25
+# scientist50
+
+s="Emma25 is Data scientist50 and AI Expert"
+test=s.split(" ")
+for c in test:
+    ch=0
+    di=0
+    for char in c:
+        if char.isalpha():
+            ch += 1
+        elif char.isdigit():
+            di += 1
+    if ch>0 and di>0:
+        print(c)
