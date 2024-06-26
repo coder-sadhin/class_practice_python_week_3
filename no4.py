5edf0814d312d9592bc166a3c8490b3beee9ad95
akkas_dev <codersadhin@gmail.com>
2024-06-26 21:05:03 +0800

    no 4 problem

---
+++ no4.py
@@ -0,0 +1,25 @@
+# PYnative29@#8496
+
+
+
+
+# t="PYnative29@#8496"
+# sum=0
+# count=0
+# for char in t:
+#     if char.isdigit():
+#         count+=1
+#         s=int(char)
+#         sum+=s
+# print(sum,sum/count)
+
+t=input()
+total=0
+count=0
+for char in t:
+    if char.isdigit():
+        count+=1
+        s=int(char)
+        total+=s
+average=total/count
+print("Sum is: {} Average is {}".format(total, average))
